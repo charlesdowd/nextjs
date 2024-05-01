28be65f2c626f08255a3862c436145b6fd230687
Charles Dowd <charlie@universitycredit.com>
2024-05-01 21:08:46 +0800

    custom review 404 page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react';
 import { ParsedUrlQuery } from 'querystring';
+import { notFound } from 'next/navigation';
 
 interface IProps {
   params: {
@@ -11,6 +12,7 @@ interface IProps {
 
 const Review: FunctionComponent<IProps> = ({ params }) => {
   const { productId, reviewId } = params
+  if (parseInt(reviewId) > 1000) return notFound()
   return (
     <h1>This is review {reviewId} for product {productId}</h1>
   )
